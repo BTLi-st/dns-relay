113421a0b51d58b1c30fbac080e9081afd52b61f
BTLi-st <wangchenyu2022@bupt.edu.cn>
2024-06-25 23:36:21 +0800

    main

--- inc/standard.hpp
+++ inc/standard.hpp
@@ -3,7 +3,7 @@
 #define C_PLUS_CPLUS_STANDARD_HPP
 
 #if defined(_MSC_VER) && __cplusplus == 199711L // 检测 MSVC 编译器中 __cplusplus 是否被正确定义
-#warning "C++ Version is too old, or __cplusplus is not defined correctly in MSVC."
+#pragma message("C++ Version is too old, or __cplusplus is not defined correctly in MSVC.")
 #endif
 
 #define CHECK_C_PLUS_PLUS_STANDARD(version) \
