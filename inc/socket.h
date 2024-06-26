3a415edd55615a91c8de937d5f56a5ce708d42ed
BTLi-st <wangchenyu2022@bupt.edu.cn>
2024-06-26 15:27:19 +0800

    main

---
+++ inc/socket.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef DNS_SERVER_SOCKET_H
+#define DNS_SERVER_SOCKET_H
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "log.h"
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+class UDP_SOCKET
+{
+private:
+    Log &log;
+
+#ifdef _WIN32
+    WSADATA wsaData;
+    SOCKET s;
+    struct sockaddr_in addr;
+#else
+    int s;
+    struct sockaddr_in addr;
+#endif
+
+public:
+    UDP_SOCKET(Log &log);
+    ~UDP_SOCKET();
+
+    void bind(const char *ip, int port); // 绑定
+    void bind(std::string ip, int port); // 绑定
+    void sendto(const char *ip, int port, const char *buf, int len); // 发送
+    void sendto(const std::string ip, int port, const char *buf, int len); // 发送
+    int recvfrom(char *ip, int *port, char *buf, int len); // 接收
+    int recvfrom(std::string &ip, int &port, char *buf, int len); // 接收，不需要给 IP 预留空间
+
+    void close(); // 关闭
+};
+
+#endif // DNS_SERVER_SOCKET_H
---
+++ src/socket.cpp
@@ -0,0 +1,99 @@
+#include "socket.h"
+
+UDP_SOCKET::UDP_SOCKET(Log &log) : log(log)
+{
+#ifdef _WIN32
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        log.fatal("WSAStartup failed.");
+        throw std::runtime_error("WSAStartup failed.");
+    }
+    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (socket == INVALID_SOCKET)
+    {
+        log.fatal("Create socket failed.");
+        WSACleanup();
+        throw std::runtime_error("Create socket failed.");
+    }
+#else
+    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (s == -1)
+    {
+        log.fatal("Create socket failed.");
+        throw std::runtime_error("Create socket failed.");
+    }
+#endif
+}
+
+UDP_SOCKET::~UDP_SOCKET()
+{
+    close();
+}
+
+void UDP_SOCKET::bind(const char *ip, int port)
+{
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+    if (::bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        log.fatal("Bind failed.");
+        throw std::runtime_error("Bind failed.");
+    }
+}
+
+void UDP_SOCKET::bind(std::string ip, int port)
+{
+    bind(ip.c_str(), port);
+}
+
+void UDP_SOCKET::sendto(const char *ip, int port, const char *buf, int len)
+{
+    addr.sin_family = AF_INET; // IPv4
+    addr.sin_port = htons(port); // 端口
+    addr.sin_addr.s_addr = inet_addr(ip); // IP 地址
+    if (::sendto(s, buf, len, 0, (struct sockaddr *)&addr, sizeof(addr)) == -1) // 发送数据
+    {
+        log.fatal("Sendto failed."); // 记录错误
+        throw std::runtime_error("Sendto failed."); // 抛出异常
+    }
+}
+
+void UDP_SOCKET::sendto(const std::string ip, int port, const char *buf, int len)
+{
+    sendto(ip.c_str(), port, buf, len);
+}
+
+int UDP_SOCKET::recvfrom(char *ip, int *port, char *buf, int len)
+{
+    struct sockaddr_in addr; // 地址
+    socklen_t addr_len = sizeof(addr); // 地址长度
+    int ret = ::recvfrom(s, buf, len, 0, (struct sockaddr *)&addr, &addr_len); // 接收数据
+    if (ret == -1) // 如果接收失败
+    {
+        log.fatal("Recvfrom failed.");
+        throw std::runtime_error("Recvfrom failed.");
+    }
+    *port = ntohs(addr.sin_port); // 端口
+    strcpy(ip, inet_ntoa(addr.sin_addr)); // IP 地址
+    return ret;
+}
+
+int UDP_SOCKET::recvfrom(std::string &ip, int &port, char *buf, int len)
+{
+    char _ip[16]; // IP 地址
+    int _port; // 端口
+    int ret = recvfrom(_ip, &_port, buf, len); // 接收数据
+    ip = _ip; // IP 地址
+    port = _port; // 端口
+    return ret;
+}
+
+void UDP_SOCKET::close()
+{
+#ifdef _WIN32
+    closesocket(s);
+#else
+    ::close(s);
+#endif
+}
