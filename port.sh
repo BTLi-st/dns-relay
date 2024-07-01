e574f99f104f69c4c2c57d01d932436bc6b17b98
BTLi-st <wangchenyu2022@bupt.edu.cn>
2024-07-01 22:56:24 +0800

    main

--- port.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-iptables -t nat -A PREROUTING -p udp -d 192.168.1.100 --dport 53 -j DNAT --to-destination 127.0.0.1:12345
-iptables -t nat -A POSTROUTING -p udp -s 127.0.0.1 --sport 12345 -j SNAT --to-source 192.168.1.100:53
